02904087d1193073660887c8c9e4fcc2b41bfcbb
Daniel Dahme <dahme@qualitus.de>
2024-03-29 20:05:16 +0100

    teaser module added

---
+++ src/Content/ModernTeaserModule.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace ModuledPage\Content;
+
+use Page\Moduled\AbstractModuleContent;
+
+class ModernTeaserModule extends AbstractModuleContent {
+
+	private $servicelocator;
+
+	public function __construct() {
+		$this->servicelocator = \Base3\ServiceLocator::getInstance();
+	}
+
+	public function requiresModule() {
+		return array();
+	}
+
+	public function getName() {
+		return "modernteasermodule";
+	}
+
+	public function getHtml() {
+
+		// http://labs.zeroseven.de/architektur/html-kann-ganz-schoen-schraeg-sein/
+
+		$view = $this->servicelocator->get('view');
+		$view->setPath(DIR_PLUGIN . 'ModuledPage');
+		$view->setTemplate('Content/ModernTeaserModule.php');
+		foreach ($this->data as $tag => $content) $view->assign($tag, $content);
+		return $view->loadTemplate();
+	}
+
+}
