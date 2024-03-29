85c6b7d273cc14aa2f1ad8206ed37731cb92f9d9
Daniel Dahme <dahme@qualitus.de>
2024-03-29 20:20:50 +0100

    page added

---
+++ src/Page/Index.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace ModuledPage\Page;
+
+use ModuledPage\Page\GeneratedPage;
+
+class Index extends GeneratedPage {
+
+	public function getName() {
+		return "index";
+	}
+
+        public function getUrl() {
+                return "./";
+        }
+
+}
