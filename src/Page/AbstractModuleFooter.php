8243bf0ab235dff69611908066a54d289e4e141c
Daniel Dahme <info@base3.de>
2025-05-21 02:29:33 +0200

    abstract footer added

---
+++ src/Page/AbstractModuleFooter.php
@@ -0,0 +1,7 @@
+<?php declare(strict_types=1);
+
+namespace ModuledPage\Page;
+
+use Base3\Page\Api\IPageModuleFooter;
+
+abstract class AbstractModuleFooter extends AbstractModule implements IPageModuleFooter {}
